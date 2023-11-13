849edb0241b14f5e040e04aaeb3b467c24ef5116
meddhiazoghlami <mohameddhia.zoghlami@target-energysolutions.com>
2023-11-13 16:04:03 +0100

    end to end testing for 3 endpoints:deposit,withdraw,getBalance

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/shopspring/decimal v1.3.1
+	github.com/stretchr/testify v1.8.4
 )
 
 require (
---
+++ main_test.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"assignmentProject/db"
+	"assignmentProject/server"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetBalanceRoute(t *testing.T) {
+	db := db.BuildDBConfig()
+	defer db.Close()
+	server := &server.Server{
+		Db: db,
+	}
+	router := server.SetupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/users/872a9d57-4a4c-49be-a447-5f44d3176f72/wallets/4a40cb9b-fe20-470c-96b5-ec57f12970e2/balance", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, string("{\"balance\":\"3000\"}"), w.Body.String())
+}
+
+func TestMakeDepositRoute(t *testing.T) {
+	db := db.BuildDBConfig()
+	defer db.Close()
+	server := &server.Server{
+		Db: db,
+	}
+	router := server.SetupRouter()
+
+	type MockAmount struct {
+		Amount int `json:"amount"`
+	}
+
+	reqBody := &MockAmount{
+		Amount: 1200,
+	}
+
+	jsonData, err := json.Marshal(reqBody)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/users/872a9d57-4a4c-49be-a447-5f44d3176f72/wallets/4a40cb9b-fe20-470c-96b5-ec57f12970e2/deposit", bytes.NewBuffer(jsonData))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, string("{\"message\":\"Your deposit is done successfully\",\"status\":200}"), w.Body.String())
+}
+
+func TestMakeWithdrawRoute(t *testing.T) {
+	db := db.BuildDBConfig()
+	defer db.Close()
+	server := &server.Server{
+		Db: db,
+	}
+	router := server.SetupRouter()
+
+	type MockAmount struct {
+		Amount int `json:"amount"`
+	}
+
+	reqBody := &MockAmount{
+		Amount: 1200,
+	}
+
+	jsonData, err := json.Marshal(reqBody)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/users/872a9d57-4a4c-49be-a447-5f44d3176f72/wallets/4a40cb9b-fe20-470c-96b5-ec57f12970e2/withdraw", bytes.NewBuffer(jsonData))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, string("{\"message\":\"Your withdraw is done successfully\",\"status\":200}"), w.Body.String())
+}
