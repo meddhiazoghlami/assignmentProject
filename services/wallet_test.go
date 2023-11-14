fa0299aafa13976a9cf6949a40b95efa5fabf5b0
meddhiazoghlami <mohameddhia.zoghlami@target-energysolutions.com>
2023-11-14 15:29:51 +0100

    unit test:GetBalance changed

--- services/wallet_test.go
+++ services/wallet_test.go
@@ -3,6 +3,8 @@ package services
 import (
 	"assignmentProject/db"
 	"testing"
+
+	"github.com/shopspring/decimal"
 )
 
 func TestGetBalance(t *testing.T) {
@@ -12,8 +14,13 @@ func TestGetBalance(t *testing.T) {
 	user_id := "41582010-aefd-4a2b-a452-141f5688ff36"
 	wallet_id := "4a40cb9b-fe20-470c-96b5-ec57f12970e2"
 
-	_, err := GetBalance(db, wallet_id, user_id)
+	got, err := GetBalance(db, wallet_id, user_id)
 	if err != nil {
 		t.Errorf("Test Failed with error %q", err)
+	}
+	want := 400
+	decimalValue := decimal.NewFromInt(int64(want))
+	if !got.Balance.Equal(decimalValue) {
+		t.Errorf("wanted: %q, got: %q", got.Balance, decimalValue)
 	}
 }
