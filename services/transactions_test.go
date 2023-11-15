fb9c8dd6a02c4849fa260c8b353805a7096c6659
meddhiazoghlami <mohameddhia.zoghlami@target-energysolutions.com>
2023-11-15 11:40:06 +0100

    update test:MakeDeposit/MakeWithdraw

--- services/transactions_test.go
+++ services/transactions_test.go
@@ -3,7 +3,6 @@ package services
 import (
 	"assignmentProject/db"
 	"context"
-	"fmt"
 	"testing"
 	"time"
 
@@ -15,20 +14,32 @@ func TestMakeDeposit(t *testing.T) {
 	defer cancel()
 	db := db.BuildDBConfig()
 	defer db.Close()
+	user_id := "41582010-aefd-4a2b-a452-141f5688ff36"
 	wallet_id := "4a40cb9b-fe20-470c-96b5-ec57f12970e2"
+	wallet, bErr := GetBalance(db, wallet_id, user_id)
+	if bErr != nil {
+		t.Errorf("Test Failed with error %q, caused by getting balance", bErr)
+		return
+	}
+	balance := wallet.Balance
 	amount := 4200
 	decimalValue := decimal.NewFromInt(int64(amount))
 	err := MakeDeposit(ctx, db, wallet_id, decimalValue)
 	if err != nil {
 		t.Errorf("Test Failed with error %q", err)
 		return
-	} else {
-		//in case of success - withdraw the amount
-		errorW := MakeWithdraw(ctx, db, wallet_id, decimalValue)
-		if errorW != nil {
-			fmt.Println("Something went wrong in withdrawing")
-			return
-		}
+	}
+
+	newWallet, nbErr := GetBalance(db, wallet_id, user_id)
+	if nbErr != nil {
+		t.Errorf("Test Failed with error %q, caused by getting the 2nd balance", err)
+		return
+	}
+	// Checking if newBalance = balance + amount
+	want := balance.Add(decimalValue)
+	got := newWallet.Balance
+	if !got.Equal(want) {
+		t.Errorf("got %q, wanted %q", got, want)
 	}
 }
 
@@ -37,19 +48,31 @@ func TestMakeWithdraw(t *testing.T) {
 	defer cancel()
 	db := db.BuildDBConfig()
 	defer db.Close()
+	user_id := "41582010-aefd-4a2b-a452-141f5688ff36"
 	wallet_id := "4a40cb9b-fe20-470c-96b5-ec57f12970e2"
+	wallet, bErr := GetBalance(db, wallet_id, user_id)
+	if bErr != nil {
+		t.Errorf("Test Failed with error %q, caused by getting balance", bErr)
+		return
+	}
+	balance := wallet.Balance
 	amount := 4200
 	decimalValue := decimal.NewFromInt(int64(amount))
 	err := MakeWithdraw(ctx, db, wallet_id, decimalValue)
 	if err != nil {
 		t.Errorf("Test Failed with error %q", err)
 		return
-	} else {
-		//in case of success - withdraw the amount
-		errorW := MakeDeposit(ctx, db, wallet_id, decimalValue)
-		if errorW != nil {
-			fmt.Println("Something went wrong in depositing")
-			return
-		}
+	}
+
+	newWallet, nbErr := GetBalance(db, wallet_id, user_id)
+	if nbErr != nil {
+		t.Errorf("Test Failed with error %q, caused by getting the 2nd balance", err)
+		return
+	}
+	// Checking if newBalance = balance - amount
+	want := balance.Sub(decimalValue)
+	got := newWallet.Balance
+	if !got.Equal(want) {
+		t.Errorf("got %q, wanted %q", got, want)
 	}
 }
